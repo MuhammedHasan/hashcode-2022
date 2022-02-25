4221ce3bcbe52b6decbcc14212b1f67ee69024e0
M. Hasan Celik <muhammedhasancelik@gmail.com>
2022-02-24 20:11:12 -0800

    cleaned up

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from collections import namedtuple, defaultdict
 
 class Contributor:
 
-    def __init__(self, name, skills=None):
+    def __init__(self, name):
         self.name = name
-        self.skills = skills or dict()
+        self.skills = defaultdict(int)
         self.busy_until = 0
 
     def add_skill(self, name, level):
@@ -16,30 +16,37 @@ class Contributor:
         if self.skills[role.skill] <= role.level:
             self.skills[role.skill] += 1
 
-    def valid_role(self, project, role, members, current_day):
-        if role.skill not in self.skills:
-            return False
+    def valid_role(self, role, mentor=None):
+        level = self.skills[role.skill]
 
-        if self.skills[role.skill] >= role.level:
-            if current_day >= self.busy_until:
-                return True
+        if level >= role.level:
+            return True
 
-        for _, mentor in members.items():
-            if mentor.name != self.name:
-                if role.skill in mentor.skills:
-                    if mentor.skills[role.skill] >= role.level:
-                        if self.skills[role.skill] >= role.level - 1:
-                            if current_day >= self.busy_until:
-                                return True
+        if mentor is not None:
+            if mentor.name == self.name:
+                raise ValueError('One cannot mentor your self!')
+
+            if mentor.valid_role(role):
+                if level >= role.level - 1:
+                    return True
 
         return False
 
+    def valid_time(self, project_start_day):
+        return project_start_day >= self.busy_until
+
     def assign(self, project, role, project_start_day):
+        if not self.valid_time(project_start_day):
+            raise ValueError('I am still busy cannot start on the project')
+
         self.busy_until = project_start_day + project.num_days
         self.level_up(role)
 
-    def __str__(self):
-        return f'Contributor({self.name})'
+    def __repr__(self):
+        return f'Contributor({self.name}, skills={self.skills}, busy_until={self.busy_until})'
+
+
+Role = namedtuple('Role', ['skill', 'level'])
 
 
 class Project:
@@ -58,36 +65,57 @@ class Project:
     def start_day(self):
         return self.best_before - self.num_days
 
-    def gain(self, day):
-        day_diff = day - self.best_before
+    def gain(self, project_start_day):
+        late_days = min(0, project_start_day +
+                        self.num_days - self.best_before)
+        return self.score - late_days
 
-        if day_diff <= 0:
-            return self.score
+    def cost(self):
+        return self.num_days * len(self.roles)
 
-        return self.score - day_diff
+    def __repr__(self):
+        return f'Project({self.name}, num_days={self.num_days}, score={self.score}, best_before={self.best_before}, roles={self.roles})'
 
-    def fill_roles(self, skill_contributor, current_day):
-        members = dict()  # key role_id value cont
 
+class HumanResources:
+
+    def __init__(self, contributors):
+        self.contributors = contributors
+        # TODO: index for quick and link to contributor class
+
+    def find_candidates(self, role):
+        for cont in self.contributors:
+            level = cont.skills[role.skill]
+
+            if level >= role.level - 1:
+                yield (level, cont)
+
+    def fill_roles(self, project, start_day):
+        members = dict()  # {role_id: cont}
         assigned_member = set()
 
-        for role_id, role in sorted(enumerate(self.roles), key=lambda x: -x[1][1]):
-            conts = skill_contributor[role.skill]
+        # sort by most important to least important skill
+        roles = sorted(enumerate(project.roles),
+                       key=lambda x: -x[1].level)
 
-            conts = sorted([
-                (level, cont)
-                for level, cont in conts
-                if level >= role.level
-            ], key=lambda x: (x[1].busy_until, x[0]))
+        for role_id, role in roles:
+            candidates = self.find_candidates(role)
+
+            # trade of busy vs skill
+            candidates = sorted(candidates,
+                                key=lambda x: (x[1].busy_until, x[0]))
 
             match = False
-            for _, cont in conts:
-                if cont.name in assigned_member:
+
+            for _, candidate in candidates:
+                if candidate.name in assigned_member:
                     continue
 
-                if cont.valid_role(self, role, members, current_day):
-                    members[role_id] = cont
-                    assigned_member.add(cont.name)
+                if candidate.valid_role(role) \
+                   and candidate.valid_time(start_day):
+
+                    members[role_id] = candidate
+                    assigned_member.add(candidate.name)
                     match = True
                     break
 
@@ -96,17 +124,11 @@ class Project:
 
         return [
             (members[role_id], role)
-            for role_id, role in enumerate(self.roles)
+            for role_id, role in enumerate(project.roles)
         ]
 
-    def __str__(self):
-        return f'Project({self.name})'
 
-
-Role = namedtuple('Role', ['skill', 'level'])
-
-
-def read(path):
+def read_dataset(path):
     contributors = list()
     projects = list()
 
@@ -147,100 +169,53 @@ def read(path):
     return contributors, projects
 
 
-def solve(contributors, projects):
+def solve_naive(contributors, projects):
 
-    # insan kaynaklari
-    skill_contributor = defaultdict(list)
+    hr = HumanResources(contributors)
 
-    for cont in contributors:
-        for skill, skill_level in cont.skills.items():
-            skill_contributor[skill].append((skill_level, cont))
-
-    skill_contributor = dict(skill_contributor)
-
-    for skill, conts in skill_contributor.items():
-        skill_contributor[skill] = sorted(conts, key=lambda x: -x[0])
-
-    projects = sorted(projects, key=lambda p: (
-        p.start_day, -(p.score / p.num_days)))
+    projects = sorted(projects, key=lambda p:
+                      p.gain(p.start_day) / p.cost())
 
     output = dict()
 
-    unassigned_projects = list()
-
     for project in tqdm(projects):
-        conts_roles = project.fill_roles(skill_contributor)
+        conts_roles = hr.fill_roles(project, project.start_day)
 
         if len(conts_roles) == 0:
-            # unassigned_projects.append(project)
             continue
 
+        project_start_day = max([cont.busy_until for cont, _ in conts_roles])
+
         for cont, role in conts_roles:
-            cont.assign(project, role, conts_roles.copy())
+            cont.assign(project, role, project_start_day)
 
         output[project.name] = [cont for cont, _ in conts_roles]
-
-        # for un_project in unassigned_projects:
-        #     conts_roles = un_project.fill_roles(skill_contributor)
-
-        #     if len(conts_roles) == 0:
-        #         next
-
-        #     for cont, role in conts_roles:
-        #         cont.assign(un_project, role)
-        #     output[un_project.name] = [cont for cont, _ in conts_roles]
 
     return output
 
 
-def solve_day(contributors, projects):
-    # insan kaynaklari
-    skill_contributor = defaultdict(list)
+def batch(projects, batch_size=10):
+    for i in range(len(projects)-batch_size+1):
+        for _ in range(5):  # try same batch 5 time
+            yield projects[i: i+batch_size]
 
-    for cont in contributors:
-        for skill, skill_level in cont.skills.items():
-            skill_contributor[skill].append((skill_level, cont))
 
-    skill_contributor = dict(skill_contributor)
+def solve_batch(contributors, projects):
 
-    for skill, conts in skill_contributor.items():
-        skill_contributor[skill] = sorted(conts, key=lambda x: -x[0])
+    hr = HumanResources(contributors)
 
-    # projects = sorted(projects, key=lambda p: (
-    #     p.start_day, -(p.score / p.num_days)))
-
+    projects = sorted(projects, key=lambda p: p.best_before + p.score)
     output = dict()
 
-    day = 0
+    for project_batch in tqdm(batch(projects), total=len(projects) * 5):
+        project_batch = sorted(project_batch, key=lambda p:
+                               p.gain(p.start_day) / p.cost())
 
-    assigned_projects = set()
+        for project in project_batch:
+            if project.name in output:
+                continue
 
-    num_projects = len(projects)
-
-    while len(projects) > 0:
-
-        print(max([
-            cont.busy_until
-            for cont in contributors
-        ]))
-
-        gains = [
-            (project.gain(day), project)
-            for project in projects
-        ]
-        # print([i for i, j in gains])
-
-        projects = [
-            project
-            for gain, project in gains
-            if (gain > 0) and (project.name not in assigned_projects)
-        ]
-
-        gains = sorted(gains, key=lambda x: -
-                       (x[0] / (x[1].num_days * len(x[1].roles))))
-
-        for _, project in gains:
-            conts_roles = project.fill_roles(skill_contributor, day)
+            conts_roles = hr.fill_roles(project, project.start_day)
 
             if len(conts_roles) == 0:
                 continue
@@ -251,17 +226,12 @@ def solve_day(contributors, projects):
             for cont, role in conts_roles:
                 cont.assign(project, role, project_start_day)
 
-            assigned_projects.add(project.name)
             output[project.name] = [cont for cont, _ in conts_roles]
-            break
-
-        day += 1
-        print(f'{day}/{len(projects)}/{num_projects}/{len(conts_roles)}')
 
     return output
 
 
-def write(path, solution):
+def write_solution(path, solution):
 
     with open(path, 'w') as f:
         num_projects = sum(
@@ -280,26 +250,19 @@ def write(path, solution):
 
 def main():
     files = [
-        # 'a_an_example.in.txt',
-        # 'b_better_start_small.in.txt',
-        # 'c_collaboration.in.txt',
+        'a_an_example.in.txt',
+        'b_better_start_small.in.txt',
+        'c_collaboration.in.txt',
         'd_dense_schedule.in.txt',
-        # 'e_exceptional_skills.in.txt',
-        # 'f_find_great_mentors.in.txt'
+        'e_exceptional_skills.in.txt',
+        'f_find_great_mentors.in.txt'
     ]
 
     for i in files:
-        contributors, projects = read('../input/' + i)
-
-        # print([
-        #     i.num_days
-        #     for i in projects
-        # ])
-        # print(len(projects))
-
-        solution = solve_day(contributors, projects)
-        # # solution = solve(contributors, projects)
-        write('../output/' + i, solution)
+        contributors, projects = read_dataset('../input/' + i)
+        # solution = solve_naive(contributors, projects)
+        solution = solve_batch(contributors, projects)
+        write_solution('../output/' + i, solution)
 
 
 if __name__ == "__main__":
